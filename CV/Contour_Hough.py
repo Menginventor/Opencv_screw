0249d9a583ad4c6e3ba4d522db6dbcfae15c84f7
/dhamdhawach    Horsuwan <meng.inventor@gmail.com>
2018-11-07 17:15:20 +0700

    add file

--- CV/Contour_Hough.py
+++ CV/Contour_Hough.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import os
 img = cv2.imread('screw1.jpg')
 
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -44,6 +45,15 @@ plt.xticks([]), plt.yticks([])
 img_cnts = img.copy()
 
 cv2.drawContours(img_cnts, contours, -1, (0,255,0), 3)
+folder = os.getcwd()+'/Crop_img'
+for the_file in os.listdir(folder):
+    file_path = os.path.join(folder, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+        # elif os.path.isdir(file_path): shutil.rmtree(file_path)
+    except Exception as e:
+        print(e)
 for i in range(len(contours)):
     cnt = contours[i]
     x,y,w,h = cv2.boundingRect(cnt)
@@ -68,7 +78,9 @@ for i in range(len(contours)):
         crop_xmax = width-1
 
     crop_img = edges[crop_ymin:crop_ymax, crop_xmin:crop_xmax]
-    cv2.imwrite('/crop_img/crop-'+str(i)+'.jpg',crop_img)
+    cv2.imwrite(os.getcwd()+'/Crop_img/crop-'+str(i)+'.jpg',crop_img)
+
+
 
 plt.subplot(221),plt.imshow(cv2.cvtColor(img_cnts, cv2.COLOR_BGR2RGB)),plt.title('Contours')
 plt.xticks([]), plt.yticks([])
